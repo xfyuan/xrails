47ed78f8d3abcc2aa2e064f3cd9f093b1359a115
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2016-03-14 22:30:38 +0800

    add User #show, #create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,23 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+        render json: @user, status: 201
+    else
+      render json: { errors: @user.errors }, status: 422
+    end
+  end
+
+  private
+    def user_params
+      params.required(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :authentication_token, :info)
+    end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,4 +17,54 @@ RSpec.describe UsersController, type: :controller do
       expect(assigns(:users)).to eq [user]
     end
   end
+
+  describe 'GET #show' do
+    before do
+      get :show, id: user.id
+    end
+
+    it { should respond_with 200 }
+
+    it 'assigns the requested user as @user' do
+      expect(assigns(:user)).to eq user
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid params' do
+      before do
+        post :create, user: valid_attributes
+      end
+
+      it { should respond_with 201 }
+
+      it 'create a new user' do
+        expect {
+          post :create, user: attributes_for(:user, email:'another@abc.com')
+        }.to change(User, :count).by(1)
+      end
+
+      it 'assigns new created user as @user' do
+        expect(assigns(:user)).to be_a(User)
+      end
+
+      it 'render the json response for user created' do
+        expect(json_response).to be_a(Hash)
+        expect(json_response[:email]).to eq valid_attributes[:email]
+      end
+    end
+
+    context 'with invalid params' do
+      before do
+        post :create, user: invalid_attributes
+      end
+
+      it { should respond_with 422 }
+
+      it 'renders an errors json' do
+        expect(json_response).to have_key(:errors)
+        expect(json_response[:errors][:email]).to include "can't be blank"
+      end
+    end
+  end
 end
