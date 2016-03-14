fb519eeb7b67b7afcda46701e576672496acff58
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2016-03-14 21:16:17 +0800

    improve user model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   validates_presence_of :email, :authentication_token
+  validates_uniqueness_of :email
+
   validates_length_of :email, in: 6..50
   validates_length_of :password, in: 4..30
   validates_length_of :firstname, in: 2..30
   validates_length_of :lastname, in: 2..30
+
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject(:user) { create :user }
+  subject(:user) { build :user }
 
-  it { should be_valid }
+  describe 'validation' do
+    it { should respond_to(:email) }
+    it { should respond_to(:firstname) }
+    it { should respond_to(:lastname) }
+    it { should respond_to(:password) }
+    it { should respond_to(:password_confirmation) }
+    it { should respond_to(:authentication_token) }
 
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:authentication_token) }
+    it { should be_valid }
 
-  it { should validate_length_of(:email).is_at_least(6).is_at_most(50) }
-  it { should validate_length_of(:password).is_at_least(4).is_at_most(30) }
-  it { should validate_length_of(:firstname).is_at_least(2).is_at_most(30) }
-  it { should validate_length_of(:lastname).is_at_least(2).is_at_most(30) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:authentication_token) }
+
+    it { should validate_uniqueness_of(:email) }
+
+    it { should validate_confirmation_of(:password) }
+
+    it { should validate_length_of(:email).is_at_least(6).is_at_most(50) }
+    it { should validate_length_of(:password).is_at_least(4).is_at_most(30) }
+    it { should validate_length_of(:firstname).is_at_least(2).is_at_most(30) }
+    it { should validate_length_of(:lastname).is_at_least(2).is_at_most(30) }
+
+    it { should allow_value('user@abc123.com').for(:email) }
+    it { should_not allow_value('user@abc.', 'user@foo,com', 'user@foo+bar.com').for(:email) }
+
+    it { should have_secure_password }
+
+  end
 end
