64feafc4aa7381e7411a88a61158e18c7d4e589a
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2016-03-17 00:19:15 +0800

    user authenticate_with_http_token replace authenticate_or_request_with_http_token

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,6 +1,6 @@
 module Authenticable
   def current_user
-    authenticate_or_request_with_http_token do|token, options|
+    authenticate_with_http_token do |token, options|
       @current_user ||= User.find_by(authentication_token: token)
     end
   end
